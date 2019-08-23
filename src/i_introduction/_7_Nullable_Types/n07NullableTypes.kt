162210477bb6af441a7055a40851945f7f2f0ad0
vthakur <vthakur@hotels.com>
2019-08-23 21:52:49 +0530

    done till smart casts

--- src/i_introduction/_7_Nullable_Types/n07NullableTypes.kt
+++ src/i_introduction/_7_Nullable_Types/n07NullableTypes.kt
@@ -22,10 +22,15 @@ fun todoTask7(client: Client?, message: String?, mailer: Mailer): Nothing = TODO
     references = { JavaCode7().sendMessageToClient(client, message, mailer) }
 )
 
-fun sendMessageToClient(
+fun sendMessageToClient (
     client: Client?, message: String?, mailer: Mailer
 ) {
-    todoTask7(client, message, mailer)
+    val email = client?.personalInfo?.email
+
+
+    if (message != null && email != null) {
+        mailer.sendMessage(email, message)
+    }
 }
 
 class Client(val personalInfo: PersonalInfo?)
