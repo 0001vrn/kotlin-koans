c07ecbba27cae3200f93f743e0f0b93b9c5e9f70
vthakur <vthakur@hotels.com>
2019-09-26 16:07:14 +0530

    primality test

--- src/i_introduction/_12_Extensions_On_Collections/n12ExtensionsOnCollections.kt
+++ src/i_introduction/_12_Extensions_On_Collections/n12ExtensionsOnCollections.kt
@@ -2,9 +2,10 @@ package i_introduction._12_Extensions_On_Collections
 
 import util.TODO
 import util.doc12
+import java.lang.StringBuilder
 
 fun todoTask12(): Nothing = TODO(
-    """
+        """
         Task 12.
         Kotlin standard library contains lots of extension functions that make the work with collections more convenient.
         Rewrite the previous example once more using an extension function 'sortedDescending'.
@@ -13,10 +14,37 @@ fun todoTask12(): Nothing = TODO(
         Thus in Kotlin we don't introduce our own collections, but use standard Java ones (slightly improved).
         Read about read-only and mutable views on Java collections.
     """,
-    documentation = doc12()
+        documentation = doc12()
 )
 
 fun task12(): List<Int> {
     return arrayListOf(1, 5, 2).sorted().asReversed()
 }
 
+fun isPrime(number: Int): Boolean {
+    for (i in 2 until number)
+        if (number % i == 0)
+            return false
+    return true
+}
+
+
+fun getPrimes(number: String): List<Int> {
+    val num = number.toInt(10)
+    val primes = ArrayList<Int>()
+
+    for (i in 2..num)
+        when(isPrime(i)) {
+            true -> primes.add(i)
+        }
+
+    return primes
+}
+
+fun primesAsString(number: String): String {
+    var sb = StringBuilder("")
+    for (prime in getPrimes(number))
+        sb.append("$prime ")
+
+    return sb.toString()
+}
